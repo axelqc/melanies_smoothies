4d41b0e56fcad9443ec99373a8445b4f43414160
Axel Quiroga Caldera <87676595+axelqc@users.noreply.github.com>
2025-06-07 17:54:50 -0600

    Update streamlit_app.py
    add section with api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -37,4 +37,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
-             
+
+# display nutrition information via API
+smoothieroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothieroot_response)
